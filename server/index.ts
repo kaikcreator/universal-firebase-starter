81eb1758e49509254b1daec15b6b1f165c7cbfd8
-KaiK- <enriqueoriol@gmail.com>
2018-04-04 18:40:31 +0200

    Added missing configuration to index.ts in order to export SSR variable. Renamed index.html to index-1.html to prevent firebase from serving the static file instead of calling the cloud function Static express server moved to DISABLED_FIREBASE space, as firebase hosting serves static files

--- server/index.ts
+++ server/index.ts
@@ -8,20 +8,26 @@ import  *  as  express  from  'express';
 import { join } from  'path';
   
 // NOTE: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } =  require('./main.bundle');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } =  require('./server/main.bundle');
 
 // NgUniversalTools: Express Engine and moduleMap for lazy loading
 import { ngExpressEngine } from  '@nguniversal/express-engine';
 import { provideModuleMap } from  '@nguniversal/module-map-ngfactory-loader';
 
+//firebase cloud functions
+import * as firebaseFunctions from 'firebase-functions';
+
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
+//check if Firebase functions is enabled or not
+const DISABLE_FIREBASE = process.env.DISABLE_FIREBASE || false;
+
 // Express server
 const  app  =  express();
 const  PORT  =  process.env.PORT  ||  4000;
-const  DIST_FOLDER  =  join(process.cwd(), 'dist');
+const DIST_FOLDER = join(process.cwd(), DISABLE_FIREBASE ? 'dist' : './');
 
 app.engine('html', ngExpressEngine({
 bootstrap:  AppServerModuleNgFactory,
@@ -31,7 +37,7 @@ providers: [
 }));
 
 app.set('view engine', 'html');
-app.set('views', DIST_FOLDER);
+app.set('views', join(DIST_FOLDER, 'browser'));
 
 /* TODO: implement data requests securely
 // app.get('/api/*', (req, res) => {
@@ -39,15 +45,20 @@ app.set('views', DIST_FOLDER);
 // });
 */
 
-// Server static files from browser
-app.get('*.*', express.static(DIST_FOLDER));
-
 // All regular routes use the Universal engine
 app.get('*', (req, res) => {
-res.render(join(DIST_FOLDER, 'index.html'), { req });
+    res.render(join(DIST_FOLDER, 'browser', 'index-1.html'), {req});
 });
 
-// Start up the Node server
-app.listen(PORT, () => {
-console.log(`Node server listening on http://localhost:${PORT}`);
-});
+if(DISABLE_FIREBASE){
+    // Server static files from express in case there's no firebase hosting
+    app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+
+    // Start up the Node server if not using firebase cloud functions
+    app.listen(PORT, () => {
+        console.log(`Node server listening on http://localhost:${PORT}`);
+    });
+}
+
+//server side rendering using frebase cloud functions
+export let ssr = DISABLE_FIREBASE ? null : firebaseFunctions.https.onRequest(app);
